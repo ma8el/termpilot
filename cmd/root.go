b52e0987a93dc8b078c56db46cdfc40c33854e94
Markus Kurbel <m4rku@pm.me>
2025-02-07 08:08:29 +0100

    Add basic project structure

---
+++ cmd/root.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile string
+	rootCmd = &cobra.Command{
+		Use:   "termpilot",
+		Short: "Termpilot is a terminal based AI agent",
+	}
+)
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.termpilot.yaml)")
+	rootCmd.PersistentFlags().String("model", "gpt-4o-mini", "model to use")
+	viper.BindPFlag("model", rootCmd.PersistentFlags().Lookup("model"))
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatalf("Failed to get user home directory: %v", err)
+		}
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".termpilot")
+		viper.SetConfigType("yaml")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			log.Fatalf("Failed to read config file: %v", err)
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"termpilot/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
